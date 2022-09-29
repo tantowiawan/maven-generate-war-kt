d82d03f46bf545ba2005940d174fee280967927e
tantowiawan <tantowibudi23@gmail.com>
2022-09-29 10:46:33 +0700

    Update Author.kt

--- src/main/kotlin/com/test/mvn/entity/Author.kt
+++ src/main/kotlin/com/test/mvn/entity/Author.kt
@@ -17,3 +17,24 @@ class Author {
 interface AuthorRepository : JpaRepository<Author, Int?>{
 
 }
+
+/*
+    @GetMapping("getName")
+    fun servantName(
+        req: HttpServletRequest,
+        @RequestParam name: List<String>,
+        @PageableDefault pageable: Pageable
+    ): ResponseEntity<Any> {
+        val specS : Specification<Servant?> = Specification<Servant?> { root: Root<Servant?>, query: CriteriaQuery<*>?, criteriaBuilder: CriteriaBuilder ->
+            root.get<String>("name").`in`(name.toMutableList())
+        }
+
+        val spec2 : Specification<Servant?> = Specification<Servant?> { root: Root<Servant?>, query: CriteriaQuery<*>?, criteriaBuilder: CriteriaBuilder ->
+            criteriaBuilder.like(
+                criteriaBuilder.lower(root.get<String>("name").`as`(String::class.java)),
+                "%bu%"
+            )
+        }
+        return ResponseEntity.ok(servantRepository.findAll(specS.or(spec2), pageable))
+//        return ResponseEntity.ok(servantRepository.findAll(pageable))
+    }*/
